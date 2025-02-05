1acf8be573be0c6e3d9670cda5904e170ffaf9f0
Divyansh Patel <89719936+Divyansh089@users.noreply.github.com>
2025-02-05 22:13:54 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <div className='h-[50vh] z-[100] relative bg-black flex items-center justify-center text-center' >
-        <a href='#' target="_blank" className='text-white text-2xl'>Apple ui clone made by <span className="text-green cursor-pointer">@Divyansh Patel</span></a>
+        <a href="https://x.com/DPatel8085" target="_blank" className='text-white text-2xl'>Apple ui clone made by <span className="text-green cursor-pointer">@DPatel8085</span></a>
     </div>
   )
 }
