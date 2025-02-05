0ba0614c696a82d8606e96fa8b6d0640b29540b4
Divyansh Patel <pateldivyansh131@gmail.com>
2025-02-05 21:37:07 +0530

    Initial commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 function Footer() {
   return (
     <div className='h-[50vh] z-[100] relative bg-black flex items-center justify-center text-center' >
-        <a href='https://twitter.com/arshadyaseeen' target="_blank" className='text-white text-2xl'>Apple ui clone made by <span className="text-green cursor-pointer">@arshadyaseeen</span></a>
+        <a href='#' target="_blank" className='text-white text-2xl'>Apple ui clone made by <span className="text-green cursor-pointer">@Divyansh Patel</span></a>
     </div>
   )
 }
